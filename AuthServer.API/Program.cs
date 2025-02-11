817b0847ed58d2982e69742db360d034a199a393
gencaydo <gdoyurucu@hotmail.com>
2025-02-11 21:03:04 +0300

    Fixed Some errors

--- AuthServer.API/Program.cs
+++ AuthServer.API/Program.cs
@@ -98,7 +98,8 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("LocalhostPolicy", policy =>
     {
-        policy.WithOrigins("http://angulartest.gencaydoyurucu.com.tr/") // Specify your localhost URL and port (e.g., React or Angular app)
+        policy.WithOrigins("http://angulartest.gencaydoyurucu.com.tr",
+                            "http://localhost:4200") // Specify your localhost URL and port (e.g., React or Angular app)
               .AllowAnyHeader()
               .AllowAnyMethod()
               .AllowCredentials();
